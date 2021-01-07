026152facc45ffa5516b66aee2c13f09c57fafc9
Philip Cutler <greenthegarden@gmail.com>
2021-01-07 22:19:29 +1030

    updated supervisord.conf for deployment

--- install-dependencies.sh
+++ install-dependencies.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # ensure required python modules are installed
-sudo apt-get install python3-distutils python3-apt
+sudo apt-get install python3-distutils python3-apt python3-pip
 
 # ensure python is set to python3
 sudo update-alternatives --install /usr/bin/python python /usr/bin/python2 1
@@ -9,3 +9,6 @@ sudo update-alternatives --install /usr/bin/python python /usr/bin/python3 2
 
 # install poetry
 curl -sSL https://raw.githubusercontent.com/python-poetry/poetry/master/get-poetry.py | python -
+
+# install supervisor
+python -m pip install --upgrade supervisor
