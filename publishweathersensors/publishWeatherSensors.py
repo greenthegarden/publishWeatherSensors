4bdf56f3da695f803008db5f9d96953ddb19e91c
Philip Cutler <greenthegarden@gmail.com>
2021-04-30 22:04:00 +0930

    added input code

--- publishweathersensors/publishWeatherSensors.py
+++ publishweathersensors/publishWeatherSensors.py
@@ -240,84 +240,99 @@ def run():
   except ValidationError as err:
     print(err.json())
     
-    # # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
-    # # 146 = FT-020T WeatherRack2, #147 = F016TH SDL Temperature/Humidity Sensor
-    # print("Starting Wireless Read")
-    # #cmd = [ '/usr/local/bin/rtl_433', '-vv',  '-q', '-F', 'json', '-R', '146', '-R', '147']
-    # cmd = ['/usr/local/bin/rtl_433', '-q',
-    #        '-F', 'json', '-R', '146', '-R', '147']
+    # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # 146 = FT-020T WeatherRack2, #147 = F016TH SDL Temperature/Humidity Sensor
+    print("Starting Wireless Read")
+    #cmd = [ '/usr/local/bin/rtl_433', '-vv',  '-q', '-F', 'json', '-R', '146', '-R', '147']
+    cmd = ['/usr/local/bin/rtl_433', '-q',
+           '-F', 'json', '-R', '146', '-R', '147']
 
-    # #   We're using a queue to capture output as it occurs
-    # try:
-    #     from Queue import Queue, Empty
-    # except ImportError:
-    #     from queue import Queue, Empty  # python 3.x
-    # ON_POSIX = 'posix' in sys.builtin_module_names
+    #   We're using a queue to capture output as it occurs
+    try:
+        from Queue import Queue, Empty
+    except ImportError:
+        from queue import Queue, Empty  # python 3.x
+    ON_POSIX = 'posix' in sys.builtin_module_names
 
-    # def enqueue_output(src, out, queue):
-    #     for line in iter(out.readline, b''):
-    #         queue.put((src, line))
-    #     out.close()
+    def enqueue_output(src, out, queue):
+        for line in iter(out.readline, b''):
+            queue.put((src, line))
+        out.close()
 
-    # #   Create our sub-process...
-    # #   Note that we need to either ignore output from STDERR or merge it with STDOUT due to a limitation/bug somewhere under the covers of "subprocess"
-    # #   > this took awhile to figure out a reliable approach for handling it...
-    # p = Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1, close_fds=ON_POSIX)
-    # q = Queue()
+    #   Create our sub-process...
+    #   Note that we need to either ignore output from STDERR or merge it with STDOUT due to a limitation/bug somewhere under the covers of "subprocess"
+    #   > this took awhile to figure out a reliable approach for handling it...
+    p = Popen(cmd, stdout=PIPE, stderr=STDOUT, bufsize=1, close_fds=ON_POSIX)
+    q = Queue()
 
-    # t = Thread(target=enqueue_output, args=('stdout', p.stdout, q))
+    t = Thread(target=enqueue_output, args=('stdout', p.stdout, q))
 
-    # t.daemon = True  # thread dies with the program
-    # t.start()
+    t.daemon = True  # thread dies with the program
+    t.start()
 
-    # # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
+    # ---------------------------------------------------------------------------------------------------------------------------------------------------------------
 
-    # pulse = 0
-    # while True:
-    #     #   Other processing can occur here as needed...
-    #     #sys.stdout.write('Made it to processing step. \n')
+    pulse = 0
+    while True:
+        #   Other processing can occur here as needed...
+        #sys.stdout.write('Made it to processing step. \n')
 
-    #     try:
-    #         src, line = q.get(timeout=1)
-    #         # print(line.decode())
-    #     except Empty:
-    #         pulse += 1
-    #     else:  # got line
-    #         pulse -= 1
-    #         sLine = line.decode()
-    #         data = dict()
-    #         topic = ""
-    #         # print(sLine)
-    #         #   See if the data is something we need to act on...
-    #         if ((sLine.find('F007TH') != -1) or (sLine.find('F016TH') != -1)):
-    #             sys.stdout.write('WeatherSense Indoor T/H F016TH Found' + '\n')
-    #             # data = parseF016TH(sLine)
-    #             schema = desert.schema(reportF016TH, meta={"unknown": EXCLUDE})
-    #             try:
-    #                 sys.stdout.write(sLine + '\n')
-    #                 data = schema.load(json.loads(sLine)).to_reportIndoorSensor()
-    #                 topic = '/'.join(['weathersense', 'indoorth',
-    #                                  str(data.get('channel'))])
-    #             except ValidationError as err:
-    #                 print(err.messages)
-    #                 print(err.valid_data)
-    #         if ((sLine.find('FT0300') != -1) or (sLine.find('FT020T') != -1)):
-    #             sys.stdout.write(
-    #                 'WeatherSense WeatherRack2 FT020T found' + '\n')
-    #             # data = parseFT020T(sLine)
-    #             schema = desert.schema(reportFT020T, meta={"unknown": EXCLUDE})
-    #             try:
-    #                 sys.stdout.write(sLine + '\n')
-    #                 data = parseFT020T.parse_raw(json.loads(
-    #                     sLine)).to_reportWeatherSensor()
-    #                 topic = '/'.join(['weathersense', 'weatherrack2',
-    #                              str(data.get('device'))])
-    #             except ValidationError as err:
-    #                 print(err.messages)
-    #                 print(err.valid_data)
-    #         if topic:
-    #             sys.stdout.write(json.dumps(data) + '\n')
-    #             publish.single(topic.lower(), json.dumps(
-    #                 data), hostname=cfg.broker_ip)
+        try:
+            src, line = q.get(timeout=1)
+            # print(line.decode())
+        except Empty:
+            pulse += 1
+        else:  # got line
+            pulse -= 1
+            sLine = line.decode()
+            data = dict()
+            topic = ""
+            # print(sLine)
+            #   See if the data is something we need to act on...
+            if ((sLine.find('F007TH') != -1) or (sLine.find('F016TH') != -1)):
+                sys.stdout.write('WeatherSense Indoor T/H F016TH Found' + '\n')
+                # data = parseF016TH(sLine)
+                # schema = desert.schema(reportF016TH, meta={"unknown": EXCLUDE})
+                try:
+                    sys.stdout.write(sLine + '\n')
+                    report = reportF016TH.parse_raw(sLine)
+                    data = json.loads(reportIndoorSensor(time=report.time,
+                                                         model=report.model,
+                                                         device=report.device,
+                                                         modelnumber=report.modelnumber,
+                                                         channel=report.channel,
+                                                         battery=report.battery,
+                                                         temperature=tempFtoC(
+                                                             report.temperature_F),
+                                                         humidity=report.humidity
+                                                         ).json())
+                    topic = '/'.join(['weathersense', 'indoorth',
+                                     str(data.get('channel'))])
+                except ValidationError as err:
+                    print(str(err))
+            if ((sLine.find('FT0300') != -1) or (sLine.find('FT020T') != -1)):
+                sys.stdout.write(
+                    'WeatherSense WeatherRack2 FT020T found' + '\n')
+                # data = parseFT020T(sLine)
+                schema = desert.schema(reportFT020T, meta={"unknown": EXCLUDE})
+                try:
+                    sys.stdout.write(sLine + '\n')
+                    report = reportFT020T.parse_raw(sLine)
+                    data = json.loads(reportWeatherSensor(report=report,
+                                                          dailyrainfall=daily_rainfall.update(
+                                                              report.cumulativerain),
+                                                          monthlyrainfall=monthly_rainfall.update(
+                                                              report.cumulativerain),
+                                                          annualrainfall=annual_rainfall.update(
+                                                              report.cumulativerain)
+                                                          ).json())
+                    topic = '/'.join(['weathersense', 'weatherrack2',
+                                 str(data.get('device'))])
+                except ValidationError as err:
+                    print(str(err))
+            if topic:
+                sys.stdout.write(json.dumps(data) + '\n')
+                publish.single(topic.lower(), json.dumps(
+                    data), hostname=cfg.broker_ip)
 
-    #     sys.stdout.flush()
+        sys.stdout.flush()
